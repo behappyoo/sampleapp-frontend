e6c36b73391ed48eed778d4ab5b9454c2b5630d3
dinggu <dong0__0@naver.com>
2024-11-03 17:05:53 +0900

    modify vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,8 @@ const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    host: '0.0.0.0',
-    port: 8080, 
-    disableHostCheck: true, 
+    host: '0.0.0.0', // 모든 IP에서 접근 가능
+    port: 8080, // 원하는 포트 번호로 변경
+    allowedHosts: 'all', // 모든 호스트 허용
   },
 });
